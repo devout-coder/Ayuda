2b284f1b78c372a74f740d89869e9528e49f495f
devout-coder <awessssomepro@gmail.com>
2023-01-20 22:37:24 +0530

    tts working flawlessll

--- android/app/src/main/kotlin/com/example/csi_hackathon/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/csi_hackathon/MainActivity.kt
@@ -3,69 +3,74 @@ package com.example.csi_hackathon
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.ContactsContract
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import androidx.annotation.NonNull
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 import java.lang.String
+import java.util.*
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterActivity(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+
+
+    override fun onInit(status: Int) {
+        Log.d("debugging", "tts initialized")
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.d("debugging", "The Language not supported!")
+            } else {
+                Log.d("debugging", "The Language supported!")
+            }
+        } else {
+            Log.d("debugging", "I am fucked")
+        }
+    }
+
+    private fun speakOut(text: String) {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    public override fun onDestroy() {
+        // Shutdown TTS when
+        // activity is destroyed
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+
         super.configureFlutterEngine(flutterEngine)
         GeneratedPluginRegistrant.registerWith(flutterEngine);
         var methodChannel: MethodChannel? = null
-        val mChannel = NotificationChannel("alarms", "Alarms", NotificationManager.IMPORTANCE_HIGH)
-        mChannel.description = "This pertains to all the alarms set by the user"
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(mChannel)
 
-        val b: NotificationCompat.Builder = NotificationCompat.Builder(context, "alarms")
-        val intent = Intent(Intent.ACTION_VIEW)
-        val uri: Uri =
-            Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(7057252707))
-        intent.setData(uri)
-
-        b.setSmallIcon(R.drawable.launch_background)
-            .setContentTitle("Tap me for any help during an emergency situation")
-//            .setStyle(
-//                NotificationCompat.BigTextStyle().bigText(notifString)
-//            )
-            .setPriority(NotificationCompat.PRIORITY_MAX)
-            .setFullScreenIntent(
-                PendingIntent.getActivity(
-                    context,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                ), true
-            )
-            .setAutoCancel(true)
+        tts = TextToSpeech(this, this)
 
         methodChannel = MethodChannel(
             flutterEngine.dartExecutor.binaryMessenger,
             "alarms"
         )
         methodChannel.setMethodCallHandler { call: MethodCall?, result: MethodChannel.Result? ->
-            if (call!!.method == "call") {
-                Log.d("debugging", "thish is called")
-                val inn = Intent(Intent.ACTION_VIEW)
-                val ur: Uri =
-                    Uri.withAppendedPath(
-                        ContactsContract.Contacts.CONTENT_URI,
-                        String.valueOf(7057252707)
-                    )
-                intent.setData(ur)
-                startActivity(inn)
+            if (call!!.method == "speak") {
+                Log.d("debugging", "text should be spoken now")
+                val text: String = call.argument<String>("text")!!
+                speakOut(text)
             }
-//            handleMethodCalls(context, call, result)
-//            handleNudgerCall(this, context, call, result)
         }
     }
 }
