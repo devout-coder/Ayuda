197d8d726d468c8bf515eaa9cc365db6fc8dcd93
LUNAGOK <21lunagok@gmail.com>
2023-02-28 20:11:23 +0530

    Ayuda app logo

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ayuda',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
